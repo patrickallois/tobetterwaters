a71b6e08e1d07450e5eb7f40c0d2814913695b17
Patrick Allois <patrickallois@gmail.com>
2024-01-12 13:01:27 -0500

    fix: update past shows calculation

--- js/shows.js
+++ js/shows.js
@@ -2,6 +2,8 @@
 fetch('data/shows.txt')
   .then(response => response.text())
   .then(data => {
+    const currentDate = new Date();
+
     // Split the data into individual show entries
     const shows = data.split('\n').filter(show => show.trim() !== ''); // Exclude empty lines
 
@@ -43,11 +45,10 @@ fetch('data/shows.txt')
       const yearHeading = document.createElement('h1');
       yearHeading.textContent = `${year} Show Dates`;
 
-	  // Check if the year is in the past
-	  const currentDate = new Date();
-	  if (year < currentDate.getFullYear()) {
-		yearHeading.classList.add('past-year'); // Apply the 'past-year' class for styling
-	  }	
+      // Check if the year is in the past
+      if (year < currentDate.getFullYear()) {
+        yearHeading.classList.add('past-year'); // Apply the 'past-year' class for styling
+      }	
 
       // Create a list for the shows
       const showList = document.createElement('ul');
@@ -61,8 +62,8 @@ fetch('data/shows.txt')
         const showDetails = document.createElement('p');
 
         // Check if the show date is in the past
-        const showDate = new Date(show.date);
-        if (showDate < currentDate) {
+        const showDate = new Date(show.date + "T24:00:00");
+        if (showDate <= currentDate) {
           showDetails.classList.add('past-show'); // Apply the 'past-show' class for styling
         }
 
